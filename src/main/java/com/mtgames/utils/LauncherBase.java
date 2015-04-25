97a704841b51cd51b13d1b8c717f3c2c63b1183f
Dreaded_X <tim.huizinga@gmail.com>
2015-04-26 00:12:09 +0200

    Fixed missing import

--- src/main/java/com/mtgames/utils/LauncherBase.java
+++ src/main/java/com/mtgames/utils/LauncherBase.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
