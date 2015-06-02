f16f5c4ccb2b04df81caab21392191c2947a9df1
Dreaded_X <tim.huizinga@gmail.com>
2015-06-02 21:34:24 +0200

    Added text util class

---
+++ src/main/java/com/mtgames/utils/Text.java
@@ -0,0 +1,16 @@
+package com.mtgames.utils;
+
+public class Text {
+	public static String wrap(String msg, int maxLength) {
+		int lastSpace;
+
+		for (int i = 1; i <= 16; i++) {
+			if (msg.length() > maxLength * i) {
+				lastSpace = msg.lastIndexOf(" ", maxLength * i);
+				msg = msg.substring(0, lastSpace) + "\n" + msg.substring(lastSpace + 1);
+			}
+		}
+
+		return msg;
+	}
+}
